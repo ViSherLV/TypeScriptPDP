d136e499ceead1fe1195c4e9882de944b433417f
Your Name <vishersb@gmail.com>
2020-09-27 00:28:28 +0300

    work with classes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,37 @@
-console.log('Time to get started...');
+class Department {
+    // private name: string;
+    // private id :string;
+    private employees: string[] = []
+
+    constructor(private id:string, public name:string){
+        // this.id = id
+        // this.name = n;
+    }
+
+    describe(this: Department){
+        console.log(`Department: ${this.id}:${this.name}`)
+    }
+
+    addEmployee(employee:string = "1"){
+      this.employees.push(employee)
+    }
+
+    printEmployeeInformation(){
+        console.log(this.employees.length);
+        console.log(this.employees)
+    }
+
+}
+
+const accounting = new Department('d1', 'Accounting');
+
+accounting.addEmployee("Viktor");
+accounting.addEmployee("Vito");
+
+accounting.describe();
+
+accounting.printEmployeeInformation();
+
+// const accountingCopy = {name: 'DUMMY', describe: accounting.describe};
+
+// accountingCopy.describe()
